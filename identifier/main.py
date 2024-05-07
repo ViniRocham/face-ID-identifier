54b68b32c72293db2fae24dd46f1ba740962d673
viniRocham <viniciusrocha1004@gmail.com>
2024-05-07 19:09:18 -0300

    create def captureAndSaveFace

--- identifier/main.py
+++ identifier/main.py
@@ -8,6 +8,21 @@ video = cv2.VideoCapture(0)
 
 detector = FaceDetector()
 
+def captureAndSaveFace(img, bbox, face_id):
+    if isinstance(bbox, dict) and 'bbox' in bbox:
+        x, y, w, h = bbox['bbox']
+        x, y, w, h = int(x), int(y), int(w), int(h)
+        if h > 0 and w > 0:
+            face = img[y:y+h, x:x+w]
+            cv2.imwrite(f"known_faces/face_{face_id}.jpg", face)
+            print(f"Face registered as ID {face_id}")
+            time.sleep(5)
+        else:
+            print("Invalid bounding box")
+    else:
+        print("Invalid bounding box")
+
+
 while True:
     _,img = video.read()
     img,bboxes = detector.findFaces(img,draw=True)
